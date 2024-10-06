1c6885f6e23f77fc2ceb0e2cbe05257ce56ef644
김민성 <166998868+castlekimdev@users.noreply.github.com>
2024-10-06 16:10:45 +0900

    cors 이슈 해결

---
+++ src/main/kotlin/kim/half/graduated/WebConfig.kt
@@ -0,0 +1,16 @@
+package kim.half.graduated
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**") // 모든 엔드포인트에 대해 CORS 허용
+            .allowedOrigins("*") // React 웹앱의 주소
+            .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
+            .allowedHeaders("*") // 모든 헤더 허용
+            .allowCredentials(true) // 쿠키나 인증 정보를 함께 보낼 수 있게 허용
+    }
+}
